4627e4ca9cd1929dcca237144b5cc010eedb067b
Athira RK <athirarr764@gmail.com>
2026-03-04 00:37:00 +0530

    feat: Added popup and overlay UI for Create Task feature

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const date = new Date();
 const dateStr = date.toDateString();
 const currentDate = document.getElementById("currDate");
 currentDate.innerHTML = dateStr;
+
+const newTask = document.querySelector(".newTask");
+const taskName = document.querySelector("#name");
+const taskTime = document.querySelector("#time");
+const overLay = document.querySelector(".overlay");
+const todoApp = document.querySelector(".todoapp");
+
+
+function createTask() {
+    overLay.classList.add('active');
+    newTask.style.display = "block";  
+}
+
+function closeTask() {
+    overLay.classList.remove('active');
+    newTask.style.display = "none";
+}
