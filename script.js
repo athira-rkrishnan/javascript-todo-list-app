8074181bc1d9711eb29690b3e4c4b4af04b29aaf
Athira RK <athirarr764@gmail.com>
2026-03-04 22:24:02 +0530

    Implement code to add task to list container

--- script.js
+++ script.js
@@ -12,29 +12,37 @@ currentDate.innerHTML = dateStr;
   });
 };  */
 
-const newTask = document.querySelector(".newTask");
-const taskName = document.querySelector("#name");
-const taskTime = document.querySelector("#time");
 const overLay = document.querySelector(".overlay");
+const newTask = document.querySelector(".newTask");
+const inputName = document.querySelector("#name");
+const inputTime = document.querySelector("#time");
+
 const todoApp = document.querySelector(".todoapp");
+const tListName = document.getElementById("tName");
+const tListTime = document.getElementById("tTime");
+const listContainer = document.querySelector(".listContainer");
 
-
+   
 function createTask() {
     overLay.classList.add('active');
     newTask.style.display = "block";  
 }
 
-function closeTask(val) {
-     
+function closeTask() {
     overLay.classList.remove('active');
     newTask.style.display = "none";
-    taskName.value = "";
-    taskTime.value = "";
-   
+    inputName.value = "";
+    inputTime.value = ""; 
 }
 
 function addTask() {
-    if(taskName.value === '' || taskTime.value === '') {
+    const inNameValue = inputName.value;
+    console.log(inNameValue);
+    const inTimeValue = inputTime.value;
+    console.log(inTimeValue);
+   
+
+    if(inputName.value === '' || inputTime.value === '') {
         let alertTxt = document.createElement("p");
         alertTxt.classList.add("alertText");
         alertTxt.textContent = "Please enter in the input fields!";
@@ -42,7 +50,44 @@ function addTask() {
         alertTxt.addEventListener('animationend', () => {
             alertTxt.style.display = 'none';
         });
+
+    }
+    else {    
+            closeTask();
+          
+      const tasksContainer = `<div class = "tabs">
+                    <span>All</span>
+                    <span>Completed</span>
+                    <span>Pending</span>
+                </div>
+                <div class = "list">
+                    <div class="checkbox-wrapper-56">
+                        <label class="checkcontainer">
+                        <input type="checkbox" id = "myCheckbox">
+                        <div class="checkmark"></div>
+                        </label>
+                    </div>
+                    <div class = "task">
+                        <label for = "myCheckbox" id = "tName">${inNameValue}</label>
+                        <p id = "tTime"><i class="fa-regular fa-clock"></i>${inTimeValue}</p>
+                    </div>
+                    <div class = "editDelSec">
+                        <div class = "actExc">
+                            <p class = "activeCir">Active</p>
+                            <i class="fa-solid fa-exclamation"></i>
+                        </div>
+                        <div class = "edDEL">
+                            <span>Edit</span>
+                            <span class = "del">Delete</span>
+                        </div>
+                    </div>`;  
+                      
+        listContainer.innerHTML += tasksContainer;
+        const tabsContainer = document.querySelector(".tabs");
+        console.log(tabsContainer);
+
     }
 }
 
 
+                   
